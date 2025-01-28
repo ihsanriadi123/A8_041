2bbca6420fc95c22cf1b46b9e6656bd1d8f45c70
ihsanriadi123 <muhammad.ihsan.ft22@mail.umy.ac.id>
2025-01-28 08:24:48 +0700

    Menyebutkan konfigurasi penting, yaitu setup Retrofit dan JSON

--- app/src/main/java/com/example/asramaapp/di/AsramaContainer.kt
+++ app/src/main/java/com/example/asramaapp/di/AsramaContainer.kt
@@ -20,6 +20,7 @@ class AsramaContainer:AppContainer{
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
         .baseUrl(baseUrl).build()
 
+
     private val mahasiswaService: MahasiswaService by lazy {
         retrofit.create(MahasiswaService::class.java)}
 
